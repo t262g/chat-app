aa65161fefa60d7bebcfabfa65c7b9c83532e97c
t262g <t262gkyoutarou@gmail.com>
2020-10-07 20:03:50 +0900

    投稿時刻の日本時間への修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,6 +6,7 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @room =Room.find(params[:room_id])
     message = Message.new(message_params)
     if message.save
       redirect_to room_messages_path(@room)
